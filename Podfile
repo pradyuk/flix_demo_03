7f5203c6aa1154da0e1d50cd0f999db89bdd5ae1
pkarpuram <pkarpuram@shapesecurity.com>
2018-09-13 13:34:05 -0700

    launch screen icon and network request

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'flix_demo_03' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for flix_demo_03
+    pod 'AlamofireImage','~>3.1'
+
+  target 'flix_demo_03Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'flix_demo_03UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ flix_demo_03/NowPlayingViewController.swift
@@ -0,0 +1,52 @@
+//
+//  NowPlayingViewController.swift
+//  flix_demo_03
+//
+//  Created by pkarpuram on 9/13/18.
+//  Copyright © 2018 Pradyumna Karpuram. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+                //This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            }else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                }
+                print(dataDictionary)
+            }
+        }
+        task.resume()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
