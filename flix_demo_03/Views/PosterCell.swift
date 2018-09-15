bb065bf333573b434f048ea83a5d5f91f383e70d
pkarpuram <pkarpuram@shapesecurity.com>
2018-09-14 17:48:07 -0700

    Fix the collection view to have three images per row (fixing the height is pending)

---
+++ flix_demo_03/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo_03
+//
+//  Created by pkarpuram on 9/14/18.
+//  Copyright © 2018 Pradyumna Karpuram. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
